bc5d669e8cfa8ef71fdd43ed55019e573cdef296
volbil <volbilnexus@gmail.com>
2022-06-09 04:20:13 +0300

    Updated config example

--- docs/config.example.py
+++ docs/config.example.py
@@ -3,8 +3,10 @@ host = "0.0.0.0"
 debug = True
 port = 4321
 
+endpoint = "http://rpcuser:rpcpassword@localhost:33440"
+
 db = {
     "provider": "sqlite",
-    "filename": "../service.db",
+    "filename": "../charts.db",
     "create_db": True
 }
--- test.py
+++
@@ -1,5 +0,0 @@
-from service.sync import sync_price
-from service.sync import sync_chain
-
-sync_price()
-# sync_chain()
